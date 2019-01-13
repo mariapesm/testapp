29ee8b2254fdbf75f9c3efa2ce00f5a6e77172ba
mariapesm <youandmecanyousee@hotmail.com>
2019-01-13 12:40:59 +0200

    Create index.css

---
+++ src/css/index.css
@@ -0,0 +1,37 @@
+@import url("/bootstrap.min.css");
+.logout{
+    display: inline-block;
+   margin-left: 5%;
+}
+.module-img{
+   max-width: 100%;
+}
+.img{
+    max-width: 200px;
+}
+.grid-block{
+    border: 1px solid silver;
+    border-radius:3%;
+  background-color:#f8f8f8;
+
+}
+.title{
+    text-align: center;
+}
+
+.liked-number{
+    display: inline-block;
+    padding-left: 2%;
+
+}
+.footer {
+   position: absolute;
+   left: 0;
+   bottom: 100px;
+   width: 100%;
+   background-color: red;
+   color: white;
+   text-align: center;
+}
+
+
